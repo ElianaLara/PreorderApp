a0b98c2a86c118c4f7ade64a6efee530f709588c
ElianaLara <c3074085@newcastle.ac.uk>
2026-01-25 00:24:46 +0000

    Add basic init and run

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import request, render_template, redirect, url_for, session, Blueprint, flash
 
-
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
     return render_template("home.html")
 
+@main.route("/preorder", methods=["GET", "POST"])
+def preorder():
+    return render_template("preorder.html")
