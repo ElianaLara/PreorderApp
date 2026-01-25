de6c433522b7602f7f52d9fbeff7af1be1f2a888
ElianaLara <c3074085@newcastle.ac.uk>
2026-01-25 00:30:55 +0000

    change routes to test preorder

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,29 @@
-from flask import request, render_template, redirect, url_for, session, Blueprint, flash
+from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
+
+menu_items = {
+    "Starter": ["Veg Platter", "Non Veg Platter", "Vegan Platter"],
+    "Main": [
+        "Railway Station Lamb Curry",
+        "Delhi Kadhai Gosht",
+        "Butter Chicken 1950s",
+        "Roadside Chicken Bhuna",
+        "Kozhi Chicken Curry",
+        "South Indian Fish Curry",
+        "Dilli Paneer Butter Masala",
+        "Grandma's Aloo Matar",
+        "Paneer Tawa Bhuna"
+    ],
+    "Dessert": [
+        "Lotus Biscoff Cheesecake",
+        "Gajar Halwa",
+        "Chocolate Fudge Cake",
+        "Chef's Gulabjamun"
+    ],
+    "Drink": ["Coke", "Mango Lassi", "Goan Zombie", "Orange Juice"]
+}
 
 @main.route("/")
 def home():
-    return render_template("preorder.html")
-
-@main.route("/preorder", methods=["GET", "POST"])
-def preorder():
-    return render_template("preorder.html")
+    return render_template("preorder.html", menu_items=menu_items)
